38fac72add23cb47ad2be0e01e4a295de90309b2
Justin Lee <justindavidlee88@gmail.com>
2015-06-24 06:59:33 -0700

    Added Github pages

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(600, 480, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 var paddle, ball;
 
 function preload() {
